2240a28c4e2c4fecd4897ff8d4528ebcd2c0da3f
Paskudas <77464661+Paskudas@users.noreply.github.com>
2023-05-30 15:40:21 +0300

    1+

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+
+// Починаэмо відстежувати подію 
+const galleryLibraryEL = document.querySelector('.gallery');
+
+
+// Додаємо зображення на сайт з {galleryItems}
+const imagesListTemplate = ({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" title="${description}" alt="${description}" />
+    </a>`;
+  };
+
+const addImg = galleryItems.map(imagesListTemplate).join('');
+galleryLibraryEL.insertAdjacentHTML("afterbegin", addImg);
+
+// Додаємо дію при кліку
+galleryLibraryEL.addEventListener('click', oneGalleryImgClick );
+
+// --------------------------------------------------------------------------------
+function oneGalleryImgClick (evt){
+    // Відміна поведінки за замовчуванням (відміна завантаження файлу)
+    evt.preventDefault();
+    const imageSelected = evt.target.getAttribute("data-source");
+
+    // Перевірка що клік на зображенні
+    if (!imageSelected){return;}
+};
+    // --------------------------------------------------------------------------------
+// Підключення SimpleLightbox (модальне вікно)
+new SimpleLightbox(".gallery a", {captionDelay: 250, showCounter:false });
